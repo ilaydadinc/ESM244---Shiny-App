a45e1decf9edd7c9a17433011ca9fe2288ad1f15
Madeline Berger <maddiemt8@gmail.com>
2020-02-19 12:10:18 -0800

    set up outline of app

---
+++ app.R
@@ -0,0 +1,62 @@
+#My first app example!
+#Maddie 
+
+#Attach packages
+library(shiny)
+library(tidyverse)
+library(here)
+
+#Read in the data
+
+
+
+#Create 'ui' = "User Interface"
+
+ui <- navbarPage("Maddie's nav bar",
+                 theme = shinytheme("cyborg"),
+                 tabPanel("Summary",
+                          h1("Some giant text"),
+                          p("Here's some regular text..."),
+                          plotOutput(outputId = "diamond_plot")),
+                 tabPanel("Burrito Builder!",
+                          sidebarLayout(
+                            sidebarPanel("Burrito Builder",
+                                         checkboxGroupInput(inputId = "diamondclarity", #change widget here
+                                                            label = "Choose some!",
+                                                            choices = c(levels(diamonds$clarity)))),
+                            mainPanel("Main panel text",
+                                      plotOutput(outputId = "diamond_plot_2")) #output
+                          )),
+                 tabPanel("Emissions Calculator"),
+                 tabPanel("Burrito Map")
+                 
+                          
+                          
+)
+
+
+#Create a 'server'
+
+server <- function(input, output){
+  #first tab
+  output$diamond_plot <- renderPlot({
+    
+    ggplot(data = diamonds, aes(x = carat, y = price))+
+      geom_point(aes(color = clarity))
+    
+  })
+  #reactive df for second tab
+  diamond_clarity <- reactive({
+    diamonds %>% 
+      filter(clarity %in% input$diamondclarity)
+  })
+  #outout for secondtab
+  output$diamond_plot_2 <- renderPlot({
+    ggplot(data = diamond_clarity(), aes(x = clarity, y = price))+
+      geom_violin(aes(fill = clarity))
+  })
+  
+}
+
+# Let R know that you want to combine the ui & server into an app: 
+shinyApp(ui = ui, server = server)
