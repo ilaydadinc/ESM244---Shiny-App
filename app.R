bbae00c68c16de7c180ea9e95cde395c8055b55a
Madeline Berger <maddiemt8@gmail.com>
2020-02-24 14:07:22 -0800

    changed names

--- app.R
+++ app.R
@@ -26,14 +26,14 @@ ui <- navbarPage("Maddie's nav bar",
                           plotOutput(outputId = "diamond_plot")),
                  tabPanel("Burrito Builder!",
                           sidebarLayout(
-                            sidebarPanel("Burrito Builder",
+                            sidebarPanel("Burrito Builder and Emissions Calculator",
                                          checkboxGroupInput(inputId = "diamondclarity", #change widget here
                                                             label = "Choose some!",
                                                             choices = c(levels(diamonds$clarity)))),
                             mainPanel("Main panel text",
                                       plotOutput(outputId = "diamond_plot_2")) #output
                           )),
-                 tabPanel("Emissions Calculator"),
+                 tabPanel("Offset Calculator"),
                  tabPanel("Burrito Map")
                  
                           
@@ -51,11 +51,13 @@ server <- function(input, output){
       geom_point(aes(color = clarity))
     
   })
+  
   #reactive df for second tab
   diamond_clarity <- reactive({
     diamonds %>% 
       filter(clarity %in% input$diamondclarity)
   })
+  
   #outout for secondtab
   output$diamond_plot_2 <- renderPlot({
     ggplot(data = diamond_clarity(), aes(x = clarity, y = price))+
