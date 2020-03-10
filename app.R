31ef928c7bf01578a64d1d2741725438c414183e
tedjiang <tedjiang94@outlook.com>
2020-03-10 11:49:41 -0700

    .

--- app.R
+++ app.R
@@ -202,7 +202,7 @@ ui <- navbarPage(
                                          )),
                             mainPanel(leafletOutput("burr_map", width = 700, height = 500))
                           )),
-  tabPanel("Team", icon = icon("users"),
+  tabPanel("Team", icon = icon("users")
            ),
                  tabPanel("References", style = "font-size:25px",align="left", icon = icon("link"),
                             shiny::HTML("<h1><b> REFERENCES </b></h1>   <h4>[1] Wernet, G., Bauer, C., Steubing, B., Reinhard, J., Moreno-Ruiz, E., and Weidema, B., 2016. The ecoinvent database version 3 (part I): overview and methodology. The International Journal of Life Cycle Assessment, [online] 21(9), pp.1218–1230. Available at: <http://link.springer.com/10.1007/s11367-016-1087-8> [Accessed February 5, 2020]. <br>
